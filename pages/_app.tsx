998c5ca53df79d8afec72c90092509364446bc33
Paavveel <pa.tomilin@gmail.com>
2022-09-07 12:47:42 +0300

    Fix warn, delete Head in the app, add Head in the document

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,28 +1,8 @@
 import type { AppProps } from 'next/app';
-import Head from 'next/head';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <title>Top Courses - топ курсов</title>
-        <meta name='description' content='Top Courses - топ курсов' />
-        <link rel='icon' href='/favicon.ico' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
-        />
-        <link
-          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
-          rel='stylesheet'
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,21 @@ import { Head, Html, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='ru'>
-      <Head />
+      <Head>
+        <title>Top Courses - топ курсов</title>
+        <meta name='description' content='Top Courses - топ курсов' />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
