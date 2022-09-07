d235c6268b8c010593d062db84de01eeac2d5e08
Pavel T <pa.tomilin@gmail.com>
2022-09-07 22:21:49 +0300

    Add SVGR and Button with svg icon draft

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,12 @@ const Home: NextPage = () => {
   return (
     <div>
       <Htag tag='h1'>Заголовок</Htag>
-      <Button appearance='primary'>Кнопка 1</Button>
-      <Button appearance='ghost'>Кнопка 2</Button>
+      <Button appearance='primary' arrow='down'>
+        Кнопка 1
+      </Button>
+      <Button appearance='ghost' arrow='right'>
+        Кнопка 2
+      </Button>
     </div>
   );
 };
