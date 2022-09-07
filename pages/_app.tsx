149e59c5c605df2f4f91d413c9557a0d259cc162
Paavveel <pa.tomilin@gmail.com>
2022-09-07 11:06:47 +0300

    Add Fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -9,6 +9,16 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Top Courses - топ курсов</title>
         <meta name='description' content='Top Courses - топ курсов' />
         <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          crossOrigin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Noto+Sans:wght@300;400;500;700&display=swap'
+          rel='stylesheet'
+        />
       </Head>
       <Component {...pageProps} />
     </>
