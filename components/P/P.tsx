97ecacd57dfa91aade0b866d388c99c351d8ea61
Paavveel <pa.tomilin@gmail.com>
2022-09-08 15:57:43 +0300

    Add a P tag component

---
+++ components/P/P.tsx
@@ -0,0 +1,18 @@
+import cn from 'classnames';
+import styles from './P.module.css';
+import { PProps } from './P.props';
+
+export const P = ({ size = 'm', children, className, ...props }: PProps) => {
+  return (
+    <p
+      className={cn(className, {
+        [styles.s]: size === 's',
+        [styles.m]: size === 'm',
+        [styles.l]: size === 'l',
+      })}
+      {...props}
+    >
+      {children}
+    </p>
+  );
+};
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import type { NextPage } from 'next';
-import { Button, Htag } from '../components';
+import { Button, Htag, P } from '../components';
 
 const Home: NextPage = () => {
   return (
@@ -11,6 +11,30 @@ const Home: NextPage = () => {
       <Button appearance='ghost' arrow='down'>
         Кнопка 2
       </Button>
+      <P size='l'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+        fugiat modi. Perferendis veniam totam possimus atque enim, nostrum
+        officia delectus natus distinctio repellendus eligendi deserunt
+        recusandae voluptas officiis nulla nesciunt dignissimos ea sit odit
+        error corrupti quis earum. Blanditiis voluptate deleniti modi dolorum
+        eos in architecto ipsa dignissimos maiores eveniet!
+      </P>
+      <P>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+        fugiat modi. Perferendis veniam totam possimus atque enim, nostrum
+        officia delectus natus distinctio repellendus eligendi deserunt
+        recusandae voluptas officiis nulla nesciunt dignissimos ea sit odit
+        error corrupti quis earum. Blanditiis voluptate deleniti modi dolorum
+        eos in architecto ipsa dignissimos maiores eveniet!
+      </P>
+      <P size='s'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique,
+        fugiat modi. Perferendis veniam totam possimus atque enim, nostrum
+        officia delectus natus distinctio repellendus eligendi deserunt
+        recusandae voluptas officiis nulla nesciunt dignissimos ea sit odit
+        error corrupti quis earum. Blanditiis voluptate deleniti modi dolorum
+        eos in architecto ipsa dignissimos maiores eveniet!
+      </P>
     </div>
   );
 };
