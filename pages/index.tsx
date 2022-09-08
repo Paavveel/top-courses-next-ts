45af865171e7a70fe43a7be5d65e5034f54e5305
Paavveel <pa.tomilin@gmail.com>
2022-09-08 09:35:32 +0300

    Fix next.config for svgr works fine

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,10 +5,10 @@ const Home: NextPage = () => {
   return (
     <div>
       <Htag tag='h1'>Заголовок</Htag>
-      <Button appearance='primary' arrow='down'>
+      <Button appearance='primary' arrow='right'>
         Кнопка 1
       </Button>
-      <Button appearance='ghost' arrow='right'>
+      <Button appearance='ghost' arrow='down'>
         Кнопка 2
       </Button>
     </div>
