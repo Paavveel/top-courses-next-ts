2008a59b95ce650c747b096f76dd081f06d89517
Pavel T <pa.tomilin@gmail.com>
2022-09-10 14:09:16 +0300

    Add Layout component styles, implement adaptive, responsive design

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,19 +1,18 @@
 import { FunctionComponent } from 'react';
 import { Footer } from './Footer';
 import { Header } from './Header';
+import styles from './Layout.module.css';
 import { LayoutProps } from './Layout.props';
 import { Sidebar } from './Sidebar';
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <>
-      <Header />
-      <main>
-        <Sidebar />
-        <div>{children}</div>
-      </main>
-      <Footer />
-    </>
+    <div className={styles.wrapper}>
+      <Header className={styles.header} />
+      <Sidebar className={styles.sidebar} />
+      <main className={styles.main}>{children}</main>
+      <Footer className={styles.footer} />
+    </div>
   );
 };
 
