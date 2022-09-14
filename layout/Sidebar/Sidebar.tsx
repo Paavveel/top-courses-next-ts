80be3c1e1c58eefce33a8bb96d0c0d50a1b4bb6b
Paavveel <pa.tomilin@gmail.com>
2022-09-14 18:02:58 +0300

    Add the Link component to the main logo

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -1,4 +1,5 @@
 import cn from 'classnames';
+import Link from 'next/link';
 import { Menu } from '../Menu';
 import styles from './Sidebar.module.css';
 import { SidebarProps } from './Sidebar.props';
@@ -6,7 +7,9 @@ import { SidebarProps } from './Sidebar.props';
 export const Sidebar = ({ className, ...props }: SidebarProps) => {
   return (
     <div className={cn(className, styles.sidebar)} {...props}>
-      <p className={styles.logo}>Top Courses</p>
+      <Link href='/'>
+        <a className={styles.logo}>Top Courses</a>
+      </Link>
       <div>Search</div>
       <Menu />
     </div>
