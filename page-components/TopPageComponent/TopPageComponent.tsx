35ff76469d6a3cd47581b14d46a1a81383169a84
Paavveel <pa.tomilin@gmail.com>
2022-09-14 11:50:48 +0300

    Complete the Advantages component and seoText, tags

--- page-components/TopPageComponent/TopPageComponent.tsx
+++ page-components/TopPageComponent/TopPageComponent.tsx
@@ -31,12 +31,27 @@ export const TopPageComponent = ({
       {firstCategory === TopLevelCategory.Courses && page.hh && (
         <HhData {...page.hh} />
       )}
+
       {page.advantages && page.advantages.length > 0 && (
         <>
           <Htag tag='h2'>Преимущества</Htag>
           <Advantages advantages={page.advantages} />
         </>
       )}
+
+      {page.seoText && (
+        <div
+          className={styles.seo}
+          dangerouslySetInnerHTML={{ __html: page.seoText }}
+        />
+      )}
+
+      <Htag tag='h2'>Получаемые навыки</Htag>
+      {page.tags.map(t => (
+        <Tag key={t} color='primary'>
+          {t}
+        </Tag>
+      ))}
     </div>
   );
 };
