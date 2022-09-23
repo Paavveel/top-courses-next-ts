0e29b337c93a752901b040827caee68dc9dbb692
Paavveel <pa.tomilin@gmail.com>
2022-09-23 14:26:44 +0300

    Add Up component draft

---
+++ components/Up/Up.tsx
@@ -0,0 +1,10 @@
+import styles from './Up.module.css';
+import UpIcon from './up.svg';
+
+export const Up = () => {
+  return (
+    <div className={styles.up}>
+      <UpIcon />
+    </div>
+  );
+};
